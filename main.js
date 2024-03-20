394ac26e37f222ff32e20646c5bdb6d25bb136e7
areesha-22 <110978549+areesha-22@users.noreply.github.com>
2024-03-21 02:24:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var hello_eric = ("hello eric, would you like a learn some phython today");
+console.log(hello_eric);
---
+++ main.ts
@@ -0,0 +1,2 @@
+let hello_eric :string =("hello eric, would you like a learn some phython today")
+console.log(hello_eric)
